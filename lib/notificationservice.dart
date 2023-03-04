34765d89ec06281e049fa523375f372d9b4cc155
icalajah <ahmad.aziz@wearesocialights.com>
2023-03-04 11:40:47 +0700

    update icon

--- lib/notificationservice.dart
+++ lib/notificationservice.dart
@@ -35,6 +35,7 @@ class NotificationService {
 
     await flutterLocalNotificationsPlugin.initialize(
       initializationSettings,
+      onDidReceiveNotificationResponse: notificationTapBackground
     );
   }
 
